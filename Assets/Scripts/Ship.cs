65c50a3ae0ed873bee8210d3bb205e0872a192ef
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2015-12-02 12:13:49 -0500

    added a camera to the ship class
    camera need to become a class to then be called by ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class Ship : MonoBehaviour 
 {
 	Move move;
-	GameObject prefab;
 	GameObject anObject;
+	GameObject anObject2;
 	// Use this for initialization
 	void Start () 
 	{
-		prefab = Resources.Load ("OrangeShip") as GameObject;
+		GameObject prefab = Resources.Load ("OrangeShip") as GameObject;
+		GameObject prefab1 = Resources.Load ("aCamera") as GameObject;
 		anObject = Instantiate(prefab);
+		anObject2 = Instantiate (prefab1);
 		anObject.transform.position = new Vector3(0.0f,0.0f,0.0f);
+		anObject2.transform.position = anObject.transform.position;
 
 		move = new Move (anObject);
 	}
@@ -21,5 +24,6 @@ public class Ship : MonoBehaviour
 
 		//Debug.Log (anObject);
 		move.shipUpdate ();
+		//add camera class that is givin the gameobject pos
 	}
 }
