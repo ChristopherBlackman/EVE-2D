9acd4c31e17600f9c214c7cacd485b1ae10753f4
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2015-12-04 00:27:44 -0500

    Camera Object Now Implemented
    Screw your green ship!!!! RAW

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class Ship : MonoBehaviour 
 {
-	Move move;
-	GameObject anObject;
-	GameObject anObject2;
+	private Move move;
+	private GameObject anObject;
+	private ShipCamera aCameraObject; 
 	// Use this for initialization
 	void Start () 
 	{
 		GameObject prefab = Resources.Load ("OrangeShip") as GameObject;
 		GameObject prefab1 = Resources.Load ("aCamera") as GameObject;
 		anObject = Instantiate(prefab);
-		anObject2 = Instantiate (prefab1);
-		anObject.transform.position = new Vector3(0.0f,0.0f,0.0f);
-		anObject2.transform.position = anObject.transform.position;
+		//Camera aCamera = Instantiate(prefab1).GetComponent<Camera> ();
+		//aCamera.transform.position = anObject.transform.position;
+		//anObject.transform.position = new Vector3(0.0f,0.0f,0.0f);
 
+		//GetComponent<Camera>() = Instantiate (prefab1);
+		//GetComponent<Camera>().transform.position = anObject.transform.position;
+		anObject.transform.position = new Vector3 (0, 0, 1);
 		move = new Move (anObject);
+		aCameraObject = new ShipCamera ();
+
 	}
 	
 	// Update is called once per frame
@@ -24,6 +29,7 @@ public class Ship : MonoBehaviour
 
 		//Debug.Log (anObject);
 		move.shipUpdate ();
+		aCameraObject.cameraUpdatePos ( new Vector2 (anObject.transform.position.x,anObject.transform.position.y));
 		//add camera class that is givin the gameobject pos
 	}
 }
---
+++ Assets/Scripts/ShipCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class ShipCamera : MonoBehaviour {
+
+	private Camera aCamera;
+	private float cameraDistance = 5.0f; 
+	// Use this for initialization
+	public ShipCamera () 
+	{
+		aCamera = Instantiate ((Resources.Load ("aCamera") as GameObject)).GetComponent<Camera> ();
+	}
+	public ShipCamera(Camera a)
+	{
+		aCamera = a;
+	}
+	
+	public void cameraUpdatePos (Vector2 a)
+	{
+		cameraDistance = cameraDistance + Input.GetAxis ("Mouse ScrollWheel");
+
+		//var camera = GetComponent<Camera> ();
+
+		//camera.orthographicSize = 1.0f;
+		if (cameraDistance < 2.0f)
+			cameraDistance = 2.0f;
+		aCamera.orthographicSize = cameraDistance;
+		aCamera.transform.position = new Vector3 (a.x,a.y ,-1.0f);
+	}
+}
